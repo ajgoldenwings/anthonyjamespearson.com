2e15fac7fc05c3fe8a4716c4b4230fca09c46876
Anthony Pearson <ajgoldenwings@gmail.com>
2026-01-10 16:40:28 -0600

    Prerender all pages

--- website/src/app/app.routes.server.ts
+++ website/src/app/app.routes.server.ts
@@ -1,9 +1,17 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { ArticleService } from './services/article.service';
+import { inject } from '@angular/core';
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'articles/:id',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const articleService = inject(ArticleService);
+      const articles = articleService.getAllArticles();
+
+      return articles.map(a => ({ id: a.id }));
+    },
   },
   {
     path: '**',
