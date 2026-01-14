f8427d9396075631a5933c5bc2ff3c99a7516c64
Anthony Pearson <ajgoldenwings@gmail.com>
2026-01-13 21:21:31 -0600

    Tweaks and update images

--- website/src/app/pages/about/about.ts
+++ website/src/app/pages/about/about.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './about.html',
 })
 export class About {
+  showBlurredImage = true;
+
+  onImageLoad(event: Event): void {
+    const img = event.target as HTMLImageElement;
+    img.classList.remove('opacity-0');
+    img.classList.add('opacity-100');
+    this.showBlurredImage = false;
+  }
 }
