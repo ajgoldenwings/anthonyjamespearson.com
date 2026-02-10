006a8e14a4b1d14b58a1f18fafcc88647bfbee3f
Anthony Pearson <ajgoldenwings@gmail.com>
2026-02-09 21:27:31 -0600

    Delete Env files

--- website/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  aws: {
-    region: 'us-east-1', // Update with your region
-    userPoolId: '', // Will be populated after CDK deploy
-    userPoolClientId: '' // Will be populated after CDK deploy
-  }
-};
